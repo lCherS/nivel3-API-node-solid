2a29a9c322a8ddfa6b0379f931a2d6c75cc0786f
--global <lucascherez.cl@gmail.com>
2024-06-25 10:44:37 -0300

    Docker compose

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,8 +3,8 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  node_env:z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  node_env: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
