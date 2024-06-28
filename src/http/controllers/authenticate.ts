1fd814eeb403ef5f8a0f74331f399f0060fb0b76
--global <lucascherez.cl@gmail.com>
2024-06-28 11:23:09 -0300

    Utiliando Factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,9 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 import { z } from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
-import { AuthenticateUseCase } from '@/use-cases/authenthicate'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 
 export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   const authenticateBodySchema = z.object({
@@ -14,8 +13,7 @@ export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
   const { email, password } = authenticateBodySchema.parse(req.body)
 
   try {
-    const userRepository = new PrismaUsersRepository()
-    const authenticateUseCase = new AuthenticateUseCase(userRepository)
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({ email, password })
   } catch (err) {
